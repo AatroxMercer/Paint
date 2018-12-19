82e0bff9389b52c2ad1918f00d4fe817e71e52c5
AatroxMercer <songbinaatrox@qq.com>
2018-12-19 17:46:19 +0800

    setting build ok

--- app/src/main/java/gq/aatrox/paint/PaintActivity.java
+++ app/src/main/java/gq/aatrox/paint/PaintActivity.java
@@ -1,12 +1,60 @@
 package gq.aatrox.paint;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 
 public class PaintActivity extends AppCompatActivity {
+
+    private boolean moving;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paint);
+        setupPaint();
     }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private void setupPaint() {
+        final PaintView paint = findViewById(R.id.paint);
+        paint.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        Log.e("action", "down");
+                        moving = false;
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        Log.e("action", "up");
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        Log.e("action", "move");
+                        if (!moving) {
+                            moving = true;
+                        }
+                        break;
+                }
+                return moving;
+            }
+        });
+
+        paint.setOnLongClickListener(new View.OnLongClickListener() {
+
+            @Override
+            public boolean onLongClick(View v) {
+                if (!moving) {
+                    Log.e("action", "long click");
+                    startActivity(new Intent(PaintActivity.this, SettingsActivity.class));
+                }
+                return true;
+            }
+        });
+    }
+
 }
---
+++ app/src/main/java/gq/aatrox/paint/SettingsActivity.java
@@ -0,0 +1,87 @@
+package gq.aatrox.paint;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.support.v7.app.ActionBar;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.MenuItem;
+
+import java.util.List;
+
+public class SettingsActivity extends AppCompatPreferenceActivity {
+
+    private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object value) {
+            String stringValue = value.toString();
+            if (preference instanceof ListPreference) {
+                ListPreference listPreference = (ListPreference) preference;
+                int index = listPreference.findIndexOfValue(stringValue);
+                preference.setSummary(index < 0 ? null : listPreference.getEntries()[index]);
+            } else {
+                preference.setSummary(stringValue);
+            }
+            return true;
+        }
+    };
+
+    private static void bindPreferenceSummaryToValue(Preference preference) {
+        preference.setOnPreferenceChangeListener(sBindPreferenceSummaryToValueListener);
+        sBindPreferenceSummaryToValueListener.onPreferenceChange(preference,
+                PreferenceManager
+                        .getDefaultSharedPreferences(preference.getContext())
+                        .getString(preference.getKey(), ""));
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setupActionBar();
+    }
+
+    private void setupActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public void onBuildHeaders(List<Header> target) {
+        loadHeadersFromResource(R.xml.setting_headers, target);
+    }
+
+    protected boolean isValidFragment(String fragmentName) {
+        return PreferenceFragment.class.getName().equals(fragmentName)
+                || StartSettingsFragment.class.getName().equals(fragmentName);
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public static class StartSettingsFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.setting_start);
+            setHasOptionsMenu(true);
+            bindPreferenceSummaryToValue(findPreference("bg_color"));
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            int id = item.getItemId();
+            if (id == android.R.id.home) {
+                startActivity(new Intent(getActivity(), SettingsActivity.class));
+                return true;
+            }
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+}
